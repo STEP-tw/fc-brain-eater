293c76c1d3a88e5cf07d831769d170c82b81bb2a
brain-eater <34330361+brain-eater@users.noreply.github.com>
2019-01-16 12:40:57 +0530

    Added navigation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const { readFile } = require("fs");
 const app = (req, res) => {
-  res.statusCode = 404;
+  res.statusCode = 202;
   let reqHandled = false;
   if (req.url == "/") {
     readFile("./src/pages/index.html", "utf-8", function(err, data) {
-      res.statusCode = 200;
       res.write(data);
       res.end();
     });
@@ -12,7 +11,6 @@ const app = (req, res) => {
   }
   if (req.url == "/style.css") {
     readFile("./src/pages/style.css", "utf-8", function(err, data) {
-      res.statusCode = 200;
       res.write(data);
       res.end();
     });
@@ -20,13 +18,21 @@ const app = (req, res) => {
   }
   if (req.url == "/resources/freshorigins.jpg") {
     readFile("./src/resources/freshorigins.jpg", function(err, data) {
-      res.statusCode = 200;
+      res.write(data);
+      res.end();
+    });
+    reqHandled = true;
+  }
+  let relateFlowerGifUrl = "/resources/animated-flower-image-0021.gif";
+  if (req.url == relateFlowerGifUrl) {
+    readFile("./src" + relateFlowerGifUrl, function(err, data) {
       res.write(data);
       res.end();
     });
     reqHandled = true;
   }
   if (!reqHandled) {
+    res.statusCode = 404;
     res.end();
   }
 };
