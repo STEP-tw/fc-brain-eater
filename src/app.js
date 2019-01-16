a92c5110562f927ffcb1bd3578da4c7f3c8321b5
brain-eater <34330361+brain-eater@users.noreply.github.com>
2019-01-16 11:49:27 +0530

    Added Header to homePage

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
+const { readFile } = require("fs");
 const app = (req, res) => {
-  console.log(req.url);
   res.statusCode = 404;
   if (req.url == "/") {
-    res.statusCode = 200;
-    res.write("hello");
+    readFile("./src/index.html", "utf-8", function(err, data) {
+      res.statusCode = 200;
+      res.write(data);
+      res.end();
+    });
   }
-  res.end();
+  if (req.url == "/style.css") {
+    readFile("./src/style.css", "utf-8", function(err, data) {
+      res.statusCode = 200;
+      res.write(data);
+      res.end();
+    });
+  }
 };
 
 // Export a function that can act as a handler
