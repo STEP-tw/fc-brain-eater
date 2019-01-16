dd621bcfff3b86e8bedff38459ef0324fbac03f4
brain-eater <34330361+brain-eater@users.noreply.github.com>
2019-01-16 13:14:19 +0530

    create links in nav to empty files

--- src/app.js
+++ src/app.js
@@ -1,40 +1,23 @@
-const { readFile } = require("fs");
+const fs = require("fs");
 const app = (req, res) => {
-  res.statusCode = 202;
-  let reqHandled = false;
+  let filePath = req.url;
+
   if (req.url == "/") {
-    readFile("./src/pages/index.html", "utf-8", function(err, data) {
+    filePath = "/pages/index.html";
+  }
+
+  fs.stat("." + filePath, function(err, stats) {
+    if (err != null) {
+      res.statusCode = 404;
+      res.end();
+      return 1;
+    }
+    fs.readFile("." + filePath, function(err, data) {
+      res.statusCode = 202;
       res.write(data);
       res.end();
     });
-    reqHandled = true;
-  }
-  if (req.url == "/style.css") {
-    readFile("./src/pages/style.css", "utf-8", function(err, data) {
-      res.write(data);
-      res.end();
-    });
-    reqHandled = true;
-  }
-  if (req.url == "/resources/freshorigins.jpg") {
-    readFile("./src/resources/freshorigins.jpg", function(err, data) {
-      res.write(data);
-      res.end();
-    });
-    reqHandled = true;
-  }
-  let relateFlowerGifUrl = "/resources/animated-flower-image-0021.gif";
-  if (req.url == relateFlowerGifUrl) {
-    readFile("./src" + relateFlowerGifUrl, function(err, data) {
-      res.write(data);
-      res.end();
-    });
-    reqHandled = true;
-  }
-  if (!reqHandled) {
-    res.statusCode = 404;
-    res.end();
-  }
+  });
 };
 
 // Export a function that can act as a handler
